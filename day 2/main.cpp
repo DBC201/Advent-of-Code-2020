daf63587dacf49873c954de20750bf0c0c9db2f1
dbc201 <denizbcakiroglu@gmail.com>
2020-12-02 14:06:51 +0300

    cleaner code

--- day 2/main.cpp
+++ day 2/main.cpp
@@ -25,24 +25,6 @@ bool part_one_valid(std::string password, std::vector<std::string> range_string,
     return count >= min && count <= max;
 }
 
-int part_one() {
-    std::fstream file;
-    file.open("../input.txt", std::fstream::in);
-    std::string line;
-    int valid_count = 0;
-    while (getline(file, line)) {
-        std::vector<std::string> tokens = split(line, ' ');
-        std::vector<std::string> range_string = split(tokens[0], '-');
-        char letter = tokens[1][0];
-        std::string password = tokens[2];
-        if (part_one_valid(password, range_string, letter)) {
-            valid_count += 1;
-        }
-    }
-    file.close();
-    return valid_count;
-}
-
 bool part_two_valid(std::string password, std::vector<std::string> range_string, char letter) {
     int index1 = std::stoi(range_string[0])-1;
     int index2 = std::stoi(range_string[1])-1;
@@ -50,26 +32,24 @@ bool part_two_valid(std::string password, std::vector<std::string> range_string,
                                         && (password[index1] != letter || password[index2] != letter);
 }
 
-int part_two() {
+int main() {
     std::fstream file;
     file.open("../input.txt", std::fstream::in);
+    std::vector<std::string> lines;
     std::string line;
-    int valid_count = 0;
+    int part_one_valid_count = 0, part_two_valid_count = 0;
     while (getline(file, line)) {
         std::vector<std::string> tokens = split(line, ' ');
-        std::vector<std::string> index_string = split(tokens[0], '-');
+        std::vector<std::string> range_string = split(tokens[0], '-');
         char letter = tokens[1][0];
         std::string password = tokens[2];
-        if (part_two_valid(password, index_string, letter)) {
-            valid_count += 1;
-        }
+        if (part_one_valid(password, range_string, letter))
+            part_one_valid_count++;
+        if (part_two_valid(password, range_string, letter))
+            part_two_valid_count++;
     }
     file.close();
-    return valid_count;
-}
-
-int main() {
-    std::cout << part_one() << std::endl;
-    std::cout << part_two() << std::endl;
+    std::cout << part_one_valid_count << std::endl;
+    std::cout << part_two_valid_count << std::endl;
     return 0;
 }
