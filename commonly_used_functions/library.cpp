bd83359e3f6a9c14e400546fcb97ef0dd4140cc7
dbc201 <denizbcakiroglu@gmail.com>
2021-01-07 15:18:02 +0300

    add flip

--- commonly_used_functions/library.cpp
+++ commonly_used_functions/library.cpp
@@ -45,3 +45,12 @@ std::vector<int> string_to_int_vector(std::vector<std::string> v) {
         to_convert.push_back(std::stoi(i));
     return to_convert;
 }
+
+std::string flip(std::string str) {
+    for (int i=0; i<str.length()/2; i++) {
+        char temp = str[i];
+        str[i] = str[str.length()-i-1];
+        str[str.length()-i-1] = temp;
+    }
+    return str;
+}
